3fe59af73ceec8db87b3f6989be4f0bcf754f5b5
anu212006 <anushkaverma212006@gmail.com>
2026-03-25 14:26:02 +0530

    UC1 IS DONE

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,16 @@
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        // UC1: Initialize Train and Display Consist Summary
+
+        String trainName = "Chennai Express";
+        int totalBogies = 0;
+
+        System.out.println("===== TRAIN CONSIST SUMMARY =====");
+        System.out.println("Train Name: " + trainName);
+        System.out.println("Total Bogies Attached: " + totalBogies);
+        System.out.println("Status: No bogies attached yet");
+
+    }
 }
